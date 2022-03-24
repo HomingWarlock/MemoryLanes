400ac0c599af37d09dd29dc2c539ed9a6c2743ae
Michael <michael6.robinson@uwe.live.ac.uk>
2022-03-24 14:12:45 +0000

    Basic Menu
    Adding basic menu with Active Code

--- MemoryLanesUnityFile/Assets/Scripts/GameController.cs
+++ MemoryLanesUnityFile/Assets/Scripts/GameController.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    public GameObject menu_test;
+
     void Start()
     {
-        
+        menu_test.SetActive(true);
     }
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Enter))
+        {
+            menu_test.SetActive(false);
+        }
     }
 }
